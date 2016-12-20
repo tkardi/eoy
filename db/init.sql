5f219ac74ef23a8dac02ad7c61d229a1b40d5d32
Tõnis Kärdi <tonis.kardi@gmail.com>
2016-12-20 07:06:09 +0200

    gtfs andmestruktuuride loomine andmebaasis. püüame pärida eelmise ja järmise peatuse ka.

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,114 @@
 ﻿create schema gtfs authorization postgres;
 comment on schema gtfs is 'Schema for GTFS data';
 
+/* Tables */
+
+create table if not exists gtfs.agency (
+    agency_id integer,
+    agency_name character varying(250),
+    agency_url character varying(250),
+    agency_timezone character varying(100),
+    agency_phone character varying(100),
+    agency_lang character varying(3)
+);
+
+alter table gtfs.agency owner to postgres;
+alter table gtfs.agency add constraint 
+    pk__agency primary key (agency_id);
+
+
+create table if not exists gtfs.calendar(
+    service_id integer,
+    monday boolean,
+    tuesday boolean,
+    wednesday boolean,
+    thursday boolean,
+    friday boolean,
+    saturday boolean,
+    sunday boolean,
+    start_date date,
+    end_date date
+);
+alter table gtfs.calendar owner to postgres;
+alter table gtfs.calendar add constraint 
+    pk__calendar primary key (service_id);
+
+
+create table if not exists gtfs.routes(
+    route_id character varying(32),
+    agency_id integer,
+    route_short_name character varying(100),
+    route_long_name character varying(250),
+    route_type smallint,
+    route_color character varying(10),
+    competent_authority character varying(100)
+);
+alter table gtfs.routes owner to postgres;
+alter table gtfs.routes add constraint 
+    pk__routes primary key (route_id);
+alter table gtfs.routes add constraint
+    fk__routes__agency foreign key (agency_id) references gtfs.agency (agency_id)
+    on update cascade on delete no action
+    deferrable initially deferred;
+
+
+create table if not exists gtfs.shapes(
+    shape_id integer,
+    shape_pt_lat numeric,
+    shape_pt_lon numeric,
+    shape_pt_sequence smallint
+);
+alter table gtfs.shapes owner to postgres;
+create unique index uidx__shapes on gtfs.shapes (shape_id, shape_pt_sequence);
+
+create table if not exists gtfs.stops (
+    stop_id integer,
+    stop_code character varying(100),
+    stop_name character varying(250),
+    stop_lat numeric,
+    stop_lon numeric,
+    zone_id integer,
+    alias character varying(250),
+    stop_area character varying(250),
+    stop_desc character varying(250),
+    lest_x numeric,
+    lest_y numeric,
+    zone_name character varying(250)
+);
+alter table gtfs.stops owner to postgres;
+alter table gtfs.stops add constraint
+    pk__stops primary key (stop_id);
+
+
+create table if not exists gtfs.trips (
+    route_id character varying(32),
+    service_id integer,
+    trip_id integer,
+    trip_headsign character varying(250),
+    trip_long_name character varying(250),
+    direction_code character varying(10),
+    shape_id integer,
+    wheelchair_accessible boolean
+);
+alter table gtfs.trips owner to postgres;
+
+
+create table if not exists gtfs.stop_times(
+    trip_id integer,
+    arrival_time character varying(8),
+    departure_time character varying(8),
+    stop_id integer,
+    stop_sequence smallint,
+    pickup_type smallint,
+    drop_off_type smallint
+);
+alter table gtfs.stop_times owner to postgres;
+alter table gtfs.stop_times add constraint
+    fk__stop_times__stops foreign key (stop_id) references gtfs.stops (stop_id)
+    on update cascade on delete no action
+    deferrable initially deferred;
+
+
 
 create or replace function gtfs.get_time_fraction(
 	trip_start varchar, trip_fin varchar, curtime varchar) 
--- db/scrap/qtests.sql
+++ db/scrap/qtests.sql
@@ -56,6 +56,25 @@
 			n.trip_id = st.trip_id and
 			n.seq = st.stop_sequence
 	),
+	prevstop as (
+		select 
+			n.trip_id, s.stop_id, s.stop_lon, s.stop_lat, s.stop_name, 
+			st.arrival_time, st.departure_time, st.stop_sequence
+		from 
+			gtfs.stops s, gtfs.stop_times st,
+			(select 
+				trip.trip_id, max(st.stop_sequence) as seq
+			from 
+				gtfs.stop_times st, trip, curtime
+			where 
+				st.trip_id = trip.trip_id and
+				st.arrival_time < curtime.ct
+			group by trip.trip_id) n
+		where 
+			s.stop_id = st.stop_id and 
+			n.trip_id = st.trip_id and
+			n.seq = st.stop_sequence
+	),	
 	shp as (
 		select shape_id, st_makeline(array_agg(shape)) as shape 
 		from (
@@ -69,11 +88,14 @@ select
 	trip.trip_id, trip.shape_id, trip.strt, trip.fin, trip.cur,
 	nextstop.stop_id, nextstop.stop_lon, nextstop.stop_lat, nextstop.stop_name, 
 	nextstop.arrival_time, nextstop.departure_time, nextstop.stop_sequence,
+	prevstop.stop_id, prevstop.stop_lon, prevstop.stop_lat, prevstop.stop_name, 
+	prevstop.arrival_time, prevstop.departure_time, prevstop.stop_sequence,
 	trip.trip_headsign, trip.trip_long_name,
 	trip.route_short_name, trip.route_long_name,
 	'#'||trip.route_color as route_color,
 	st_lineinterpolatepoint(shp.shape, trip.fraction) as pos
-from shp, trip, nextstop
+from shp, trip, nextstop, prevstop
 where 
 	trip.shape_id = shp.shape_id and 
-	nextstop.trip_id = trip.trip_id;
+	nextstop.trip_id = trip.trip_id and
+	prevstop.trip_id = trip.trip_id;
